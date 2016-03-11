05a759759ee07a80f88db32976c10aad4fc984b7
Sam Stenvall <sam.stenvall@nordsoftware.com>
2016-03-11 12:06:36 +0200

    Add support for specifying an optional branch/tag/revision in deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Sanity check
-if [ $# -ne 1 ]; then
-    echo "$0: usage: deploy.sh <environment>"
+if [ $# -lt 1 ]; then
+    echo "$0: usage: deploy.sh <environment> [ <branch|revision|tag>"
     exit 1
 fi
 
 ENVIRONMENT=$1
+export BRANCH=$2
 
 cd /vagrant
 cap $ENVIRONMENT deploy
